e7c0a54203034c2fe22fc4873a7f3d3ffbab2ef2
lfj <liao_fengji@163.com>
2020-04-02 09:32:29 +0800

    添加跳转

--- pages/index/index.js
+++ pages/index/index.js
@@ -6,4 +6,26 @@ Page({
   data: {
 
   },
+  // 左右菜单联动
+  linkage() {
+    wx.navigateTo({
+      url: '/pages/linkage/linkage'
+    })
+  },
+  // 上下菜单联动
+  upDownLinkage() {
+    wx.showToast({
+      title: '敬请期待！',
+      icon: 'none',
+      duration: 3000
+    })
+  },
+  // 左滑
+  leftSlide() {
+    wx.showToast({
+      title: '敬请期待！',
+      icon: 'none',
+      duration: 3000
+    })
+  }
 })
